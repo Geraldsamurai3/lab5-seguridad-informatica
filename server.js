50aac8f6cf473c7fece5cb740af3f0c4af7c5236
Geraldsamurai3 <andreylanza3@gmail.com>
2025-11-08 01:45:19 -0600

    news

--- server.js
+++ server.js
@@ -25,7 +25,12 @@ const authConfig = {
   secret: process.env.AUTH0_SECRET,
   baseURL: process.env.AUTH0_BASE_URL,
   clientID: process.env.AUTH0_CLIENT_ID,
-  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+  routes: {
+    callback: '/callback',
+    login: '/login',
+    logout: '/logout'
+  }
 };
 
 // Configurar sesión
